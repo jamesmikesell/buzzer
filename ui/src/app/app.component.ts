82d59784648d3ce048867e26f2a50a078d217b5a
James Mikesell <james.mikesell>
2024-05-19 17:55:44 -0400

    init: code to deploy

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppVersion } from './app-version';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'buzzer';
+  AppVersion = AppVersion;
 }
